e2375a4b00e79b8f88aede976a19cb360f664bdc
shubhapatil154 <53892562+shubhapatil154@users.noreply.github.com>
2019-08-30 15:19:35 +0530

    Update array_replace.cpp

--- array_replace.cpp
+++ array_replace.cpp
@@ -1,18 +1,30 @@
 #include<iostream>
+using namespace std;
+void rearrange(int arr[])
+{
+        for (int i=0;i<5;i++)
+        {
+            arr[i]+=(arr[arr[i]]%5)*5;
+        }
+         for (int i=0;i<5;i++)
+         {
+            arr[i]/=5;
+         }
+         for(int i=0;i<5;i++)
+         {
+             cout<<arr[i];
+         }
+}
+
+
 int main()
 {
-        int arr[10];
+        int arr[5];
         int i;
         cout<<"enter the elements\n";
         for(i=0;i<5;i++)
+        cin>>arr[i];
         rearrange(arr);
         return 0;
 }
- void rearrange(int arr[])
-{
-        for (int i=0; i < 5; i++)
-        arr[i] += (arr[arr[i]]%5)*5;
-
-         for (int i=0; i<5; i++)
-         arr[i] /= 5;
-}
+ 
